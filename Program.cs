b126c1cd55a1769b98f33fb0ab5efea36bcfe86b
semtmb72 <sem.tamburella@gmail.com>
2026-05-16 20:02:44 +0200

    Prima versione

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// string a = " With C#";
+// Console.WriteLine("Hello, World!" + a);
+
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.WriteLine($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
